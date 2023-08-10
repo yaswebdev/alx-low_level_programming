d66b6721eaaece655457391283b9b2b3febd253a
yaswebdev <yassinerami.official@gmail.com>
2023-08-10 14:02:51 -0700

    edit file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
  * main - Enter
  * Return: Always 0
  */
-int main(void) 
+int main(void)
 {
 printf("Size of char: %lu byte(s)\n", sizeof(char));
 printf("Size of int: %lu byte(s)\n", sizeof(int));
