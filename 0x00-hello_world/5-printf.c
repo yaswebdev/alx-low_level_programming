e6423b3198c9cbec1a816b57c889d5ffc64b8c32
yaswebdev <yassinerami.official@gmail.com>
2023-08-10 13:01:38 -0700

    create file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
