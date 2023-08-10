1c662c1658def8ad5d695822d331bf79e0d54eec
yaswebdev <yassinerami.official@gmail.com>
2023-08-10 12:43:06 -0700

    edit file named 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - entry
+ * return : Always 0
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle,");
+return (0);
 
-int main() {
-    puts("Programming is like building a multilingual puzzle,");
-    return 0;
 }
