f538fd1732d2dfd8bfe6d348dee4f266e5765576
yaswebdev <yassinerami.official@gmail.com>
2023-08-11 09:19:44 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char i;
+
+for (i = 'q' ; i <= 'e' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
