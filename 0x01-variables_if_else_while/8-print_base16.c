09af1d70edf9dfe2e5845581443a6799f6e67a2c
yaswebdev <yassinerami.official@gmail.com>
2023-08-11 11:26:34 -0700

    ./8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+char j;
+
+for (i = 0 ; i < 10 ; i++)
+putchar(i + '0');
+for (j = 'a' ; j <= 'f' ; j++)
+putchar(j);
+putchar('\n');
+return (0);
+}
