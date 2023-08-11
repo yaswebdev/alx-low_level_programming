97d0334949957eff4e144ded5dfdf0682381ceea
yaswebdev <yassinerami.official@gmail.com>
2023-08-11 06:03:18 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h0>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - ENTRY
+ * Return: Always 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+	printf("is positive");
+	}
+	if else(n == 0)
+	{
+	printf("is zero");
+	}
+	else
+	{
+	printf("is negative");
+	}
+	return (0);
+}
