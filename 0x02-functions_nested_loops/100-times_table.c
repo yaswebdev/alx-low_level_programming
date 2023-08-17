87e16d9ce60421717b43edf4da640d080fd1b758
yaswebdev <yassinerami.official@gmail.com>
2023-08-17 05:21:54 -0700

    edit file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,8 +2,8 @@
 
 
 /**
-* print_times_table - prints the n times table, starting with 0
-* @n: number of the times table
+*print_times_table - prints the n times table, starting with 0
+*@n: number of the times table
 */
 	void print_times_table(int n)
 	{
