e18fddfd4e12beaa21e45fc87c43207f4898b2c0
yaswebdev <yassinerami.official@gmail.com>
2023-08-17 09:33:51 -0700

    edit file positive or negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* main - ENTRY
+* Return: Always 0
+*/
+void positive_or_negative(int i)
+{
+
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
+return;
+}
