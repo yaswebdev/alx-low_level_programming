6c2589d63967662fbb9467e9be8775ec43321a8f
yaswebdev <yassinerami.official@gmail.com>
2023-08-18 12:35:13 -0700

    edit file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,2 @@
-#ifndef MAIN_H
-#define MAIN_H
 #include <stdio.h>
 int _isupper(int c);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-#endif
