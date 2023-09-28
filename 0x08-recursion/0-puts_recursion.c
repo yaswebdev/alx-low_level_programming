38d86ff5ff31071c3cec98a3ebec979356e7250b
yaswebdev <yassinerami.official@gmail.com>
2023-09-28 15:16:08 -0700

    create files for task 0 of project recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - it's a function to output what into inside .
+ * @s :  a string to output it .
+ * Return: Always 0 (success)
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+{
+_putchar('\n');
+}
+return (0);
+}
