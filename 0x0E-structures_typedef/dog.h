d51ad97d719ac76c043e666314fcde81c997306f
yaswebdev <yassinerami.official@gmail.com>
2023-10-14 10:01:07 -0700

    edir file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - structur function .
+ * @name: enter a name .
+ * @age: age of somebody .
+ * @owner: owner of something
+ */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 #endif
