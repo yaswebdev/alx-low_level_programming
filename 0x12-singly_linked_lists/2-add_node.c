eba87c5e3aacd0d1bd5ed505f2e9515e5acf961f
yaswebdev <yassinerami.official@gmail.com>
2023-10-20 16:55:27 -0700

    edit file task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,21 +2,21 @@
 
 /**
  * add_node - adds a new node at the beginning of a list
- *@head: pointer to the head of the list
- *@str: string to be added
+ * @head: pointer to the head of the list
+ * @str: string to be added
  * Return: returns the address to the new element or NULL
  * if failed
-*/
+ */
 
 /*
-1. Allocate memory for a new node.
-2. If the allocation fails, return NULL.
-3. Copy the string into a new buffer.
-4. If the string copy fails, free the new node and return NULL.
-5. Compute the length of the string.
-6. Set the new node’s next pointer to point to the current head of the list.
-7. Set the head of the list to point to the new node.
-8. Return a pointer to the new node.
+ * Allocate memory for a new node.
+ * If the allocation fails, return NULL.
+ * Copy the string into a new buffer.
+ * If the string copy fails, free the new node and return NULL.
+ * Compute the length of the string.
+ * Set the new node’s next pointer to point to the current head of the list.
+ * Set the head of the list to point to the new node.
+ * Return a pointer to the new node.
 */
 list_t *add_node(list_t **head, const char *str)
 {
