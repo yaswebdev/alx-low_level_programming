b677c4940758457797ea4408e9b28ab303324ce1
yaswebdev <yassinerami034@gmail.com>
2024-06-07 15:01:05 -0700

    edit file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - main function of the programme
+ * Return: always 0
+ */
+
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
