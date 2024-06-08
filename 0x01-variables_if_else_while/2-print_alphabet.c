85c3e71ded04961d3db3fbb52a30ba616fcd3ae5
yaswebdev <yassinerami034@gmail.com>
2024-06-08 15:05:06 -0700

    add file 2-alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0 (success)
+ * main - the start point of execution program
+ * Return: always 0 (successfull)
  */
+
 int main(void)
 {
-	char i;
-
-	for (i = 'a' ; i <= 'z' ; i++)
-		putchar(i);
-	putchar('\n');
-	return (0);
+char i;
+for (i = 'a' ; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
