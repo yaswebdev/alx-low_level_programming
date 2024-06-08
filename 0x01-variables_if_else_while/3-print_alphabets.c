49edbe6186b42b758e6bbc0dfaaca7bec4156095
yaswebdev <yassinerami034@gmail.com>
2024-06-08 15:16:04 -0700

    add file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the enter point of execution program 
+ * Return: always 0 (successfully)
+ */
+
+int main(void)
+{
+char lower;
+char upper;
+
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+putchar(lower);
+}
+for (upper = 'A'; upper <= 'Z'; upper++)
+{
+putchar(upper);
+}
+putchar('\n');
+return (0);
+}
