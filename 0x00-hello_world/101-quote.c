7889543827dc6212c9dc2f9ccbef03b542a02d01
yaswebdev <yassinerami034@gmail.com>
2024-06-07 17:15:59 -0700

    add file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main function that is the principla function in c language
+ * Return: 0 if executed successfully and vice versa
  */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
