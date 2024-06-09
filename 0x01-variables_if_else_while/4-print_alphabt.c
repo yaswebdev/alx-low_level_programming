2abfbc6c5dbe4de5ed85fb2be79414e512d387b4
yaswebdev <yassinerami034@gmail.com>
2024-06-09 05:22:57 -0700

    add file 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0 (success)
+ * main - start point of execution program
+ * Return: always 0 (successfull)
  */
+
 int main(void)
 {
 char i;
-
-for (i = 'a' ; i <= 'z' ; i++)
+for (i = 'a'; i <= 'z'; i++)
+{
 if (i != 'q' && i != 'e')
+{
 putchar(i);
+}
+}
 putchar('\n');
 return (0);
 }
