7122e88b969fa000c56b7043a9fa425029abe7ec
yaswebdev <yassinerami034@gmail.com>
2024-06-10 07:56:19 -0700

    edit file 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,12 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
-
-    if (n < 0)
-    {
-        n = -n;
-    }
-
-    last_digit = n % 10;
-
-    _putchar('0' + last_digit);
-
-    return (last_digit);
+int last_digit;
+if (n < 0)
+{
+n = -n;
+}
+last_digit = n % 10;
+_putchar('0' + last_digit);
+return (last_digit);
 }
