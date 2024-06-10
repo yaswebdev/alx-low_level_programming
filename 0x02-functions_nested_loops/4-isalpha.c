ac1e12ff0da82f41c3d731958f3e33d43cd681b0
yaswebdev <yassinerami034@gmail.com>
2024-06-10 06:32:51 -0700

    edit file 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,6 +3,7 @@
 /**
  * _isalpha - function prototype that checks the charactere is a letter or not
  * @c: the caractere that we should check
+ * Return: 1 if c is a letter and 0 if otherwise
  */
 
 int _isalpha(int c)
