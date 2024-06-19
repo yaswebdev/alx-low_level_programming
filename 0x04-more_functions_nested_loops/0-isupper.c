c37cbe1c29466b6dbc91006f91015912bfb496da
yaswebdev <yassinerami034@gmail.com>
2024-06-19 16:09:07 -0700

    edit file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,10 +3,9 @@
 
 int _isupper(int c) {
     if (c >= 'A' && c <= 'Z') {
-        printf("%c : 1\n", c);
         return 1;
     } else if (c >= 'a' && c <= 'z') {
-        printf("%c : 0\n", c);
         return 0;
     }
+    return (-1);
 }
