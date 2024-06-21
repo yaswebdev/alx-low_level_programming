94cd8129ddb178a11dc355dce50ab6a93d2e9b33
yaswebdev <yassinerami034@gmail.com>
2024-06-20 18:13:41 -0700

    solve second problem in pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,10 @@
+#include <stdio.h>
 #include "main.h"
-/**
- * swap_int - swaps the values of two integers
- * @a: integer to swap
- * @b: integer to swap
- */
-void swap_int(int *a, int *b)
-{
-	int m;
 
-	m = *a;
+void swap_int(int *a, int *b){
+	int tmp;
+
+	tmp = *a;
 	*a = *b;
-	*b = m;
+	*b = tmp;
 }
