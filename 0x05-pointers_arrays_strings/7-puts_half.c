31fd57d2ee2a9e5542db3d435a12bae3547dba36
yaswebdev <yassinerami034@gmail.com>
2024-06-21 11:18:16 -0700

    check problem seven

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+void puts_half(char *str){
+
+
+	int length = 0;
+	int n;
+	int i;
+	while (str[length] != '\0'){
+	
+		length++;
+	
+	}
+
+	if (length % 2 == 0) {
+        	n = length / 2;
+    	} else {
+        	n = (length - 1) / 2 + 1;
+    	}
+
+	i = n;
+
+	while (str[i] != '\0'){
+		_putchar(str[i]);
+
+		i++;
+	
+	}
+	_putchar('\n');
+
+
+
+}
