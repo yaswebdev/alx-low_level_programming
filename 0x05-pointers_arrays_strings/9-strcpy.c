2707f364bf8d5b7f508eb663c6fc2dfaf21852ed
yaswebdev <yassinerami034@gmail.com>
2024-06-21 17:11:44 -0700

    solve the problem 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+char *_strcpy(char *dest, char *src){
+
+	int i = 0;
+
+	while (src[i] != '\0'){
+	
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
