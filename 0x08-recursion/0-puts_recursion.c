7249ef6475f91261e4eff27c356fdffd904a236e
yaswebdev <yassinerami034@gmail.com>
2024-08-07 04:58:47 -0700

    edit the solution

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,15 +5,9 @@
 
 void _puts_recursion(char *s){
 
+	if(*s){
+		_putchar(*s);
 
-	int i = 0;
-
-	while(s[i]){
-
-		_putchar(s[i]);
-		i++;
-	
+        	_puts_recursion(s + 1);
 	}
-	_putchar('\n');
-
 }
