24a86bcf6fde4f61142023955655f81b5f16e93a
yaswebdev <yassinerami034@gmail.com>
2024-08-07 15:02:37 -0700

    solving the problem of pow using recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+
+
+int _pow_recursion(int x, int y){
+
+	if (y == 0){
+	
+		return 1;
+	}
+	
+	if (y < 0){
+	
+		return -1;
+	}
+
+	return x * _pow_recursion(x, y - 1);
+}
